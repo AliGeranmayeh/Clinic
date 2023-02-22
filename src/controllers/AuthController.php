894ce30b79680f1b466ae87dffc223d7a1a05de1
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-22 16:00:42 +0330

    handle auth post method

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -5,11 +5,17 @@ use clinic\core\Application;
 class AuthController{
     public function login()
     {
+        if (Application::$app->request->method() === 'post') {
+            return 'handle data';
+        }
         return Application::$app->router->renderView('login');
     }
      
     public function signUp()
     {
+        if (Application::$app->request->method() === 'post') {
+            return 'handle data';
+        }
         return Application::$app->router->renderView('sign-up');
     }
 } 
