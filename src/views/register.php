799b7f0013ef2a7f942282656abcfad59d600df0
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-22 16:07:16 +0330

    add simple register form

---
+++ src/views/register.php
@@ -0,0 +1,27 @@
+
+<h1 class="my-3">register</h1>
+
+<form action = "" method = "post">
+    <div class="form-group mb-3">
+        <label for="usernmane">username</label>
+        <input type="text" class="form-control" id="usernmane" name = "username">
+    </div>
+    <div class="form-group mb-3">
+        <label for="email">email</label>
+        <input type="email" class="form-control" id="email" rows="3" name = "email">
+    </div>
+    <div class="form-group mb-3">
+        <label for="name">name</label>
+        <input type="text" class="form-control" id="name" name = "name">
+    </div>
+    <div class="form-group mb-3">
+        <label for="password">password</label>
+        <input type="password" class="form-control" id="password" rows="3" name = "password">
+    </div>
+    <div class="form-group mb-3">
+        <label for="confirm_password">confirm password</label>
+        <input type="password" class="form-control" id="confirm_password" rows="3" name = "confirm_password">
+    </div>
+    
+    <button type="submit" class="btn btn-primary my-3">register</button>
+</form>
