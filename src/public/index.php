878587d99e2880316b2e462b2d19091ff9e0f38b
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-22 15:02:56 +0330

    add doctor info router

--- src/public/index.php
+++ src/public/index.php
@@ -5,4 +5,7 @@ require_once __DIR__.'/../../vendor/autoload.php';
 
 $app = new Application();
 $app->router->get('/','home');
+$app->router->get('/doctor_info','doctor_info');
+$app->router->post('/doctor_info','doctor_info');
+
 $app->run();
