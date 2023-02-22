ab733dfde1340cdd252f67fe09e1b4d2feaf726f
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-22 16:07:25 +0330

    add simple login form

---
+++ src/views/login.php
@@ -0,0 +1,16 @@
+
+<h1 class="my-3">login</h1>
+
+<form action = "" method = "post">
+    <div class="form-group mb-3">
+        <label for="usernmane">username</label>
+        <input type="text" class="form-control" id="usernmane" name = "username">
+    </div>
+    
+    <div class="form-group mb-3">
+        <label for="password">password</label>
+        <input type="password" class="form-control" id="password" rows="3" name = "password">
+    </div>
+    
+    <button type="submit" class="btn btn-primary my-3">login</button>
+</form>
