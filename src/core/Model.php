612015a8fa20d3550a9b6bb4360d58465b17c5ec
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-22 19:08:54 +0330

    create user model and parent model

---
+++ src/core/Model.php
@@ -0,0 +1,14 @@
+<?php 
+namespace clinic\core;
+
+class Model{
+
+    public function loadData($data)
+    {
+        
+    }
+    public function validate()
+    {
+            
+    }
+}
---
+++ src/models/UsersModel.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace clinic\models;
+use clinic\core\Model;
+
+class UsersModel extends Model{
+    public String $name;
+    public String $username;
+    public String $password;
+    public String $email;
+    public String $confirm_password;
+
+    public function register()
+    {
+        return 'creating new user';
+    }
+}
