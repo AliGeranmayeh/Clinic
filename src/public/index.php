3131ab4844cd8baf09ccd2928f911cc0b77ce6f9
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-22 15:33:14 +0330

    update router based on site controller class

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,12 @@
 <?php 
 use clinic\core\Application;
+use clinic\controllers\SiteController;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
 $app = new Application();
-$app->router->get('/','home');
-$app->router->get('/doctor_info','doctor_info');
-$app->router->post('/doctor_info','doctor_info');
+$app->router->get('/',[SiteController::class,'home']);
+$app->router->get('/doctor_info',[SiteController::class,'doctorInfo']);
+$app->router->post('/doctor_info',[SiteController::class,'handleDoctorInfo']);
 
 $app->run();
