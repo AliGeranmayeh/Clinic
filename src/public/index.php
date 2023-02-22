68a220a4927f0073551018a1e3a822179c44cbc6
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-22 16:12:36 +0330

    add login and register router

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,7 @@
 <?php 
 use clinic\core\Application;
 use clinic\controllers\SiteController;
+use clinic\controllers\AuthController;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
@@ -8,5 +9,9 @@ $app = new Application();
 $app->router->get('/',[SiteController::class,'home']);
 $app->router->get('/doctor_info',[SiteController::class,'doctorInfo']);
 $app->router->post('/doctor_info',[SiteController::class,'handleDoctorInfo']);
+$app->router->get('/login',[AuthController::class,'login']);
+$app->router->post('/login',[AuthController::class,'login']);
+$app->router->get('/register',[AuthController::class,'register']);
+$app->router->post('/register',[AuthController::class,'register']);
 
 $app->run();
