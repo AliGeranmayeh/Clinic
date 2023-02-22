411d4ede3ee67ecd08229db0eae8d0fe5df8defd
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-22 20:12:44 +0330

    handle structure of errors

--- src/core/Model.php
+++ src/core/Model.php
@@ -3,10 +3,13 @@ namespace clinic\core;
 
 abstract class Model{
 
+
     public const RULE_REQUIRED = 'required';
     public const RULE_MIN = 'min';
     public const RULE_EMAIL = 'email';
     public const RULE_MATCH = 'match';
+
+    public $errors  =[];
     public function loadData($data)
     {
         
@@ -21,6 +24,33 @@ abstract class Model{
     public abstract function rules():array;
     public function validate()
     {
-            
+            foreach ($this->rules() as $attribute => $rules) {
+                $value = $this->{$attribute};
+                foreach ($rules as $ $rule) {
+                    $rule_name = $rule;
+                    if(!is_string($rule_name)){
+                        $rule_name = $rule[0];
+                    }
+                    if ($value == self::RULE_REQUIRED && !$value) {
+                        $this->addError($attribute,self::RULE_REQUIRED);
+                    }
+                }
+                    
+            }
+    }
+    public function addError($attribute , $rule_name)
+    {
+        $message = $this->errorMessages()?? '';
+        $this->errors[$attribute][] = $message;
+    }
+
+    public function errorMessages()
+    {
+        return[
+            self::RULE_REQUIRED => 'this field is requird',
+            self::RULE_MIN => 'Min length of this field must be {min}',
+            self::RULE_EMAIL => 'this field must be valid email address',
+            self::RULE_MATCH => 'this field must be as same field of {match}'
+        ];
     }
 }
