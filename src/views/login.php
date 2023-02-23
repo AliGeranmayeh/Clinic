6365f496dabaf5370225c9d1bd4e3d63d9c186c0
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-23 20:32:27 +0330

    handle register and login page bug

--- src/views/login.php
+++ src/views/login.php
@@ -3,15 +3,15 @@
 
 <form action = "" method = "post">
 
-     <div class="form-group mb-3">
-        <label for="usernmane">username</label>
-        <input type="text" class="form-control <?php echo $model->hasError('username')?'is-invalid':'' ?>" value = "<?php echo $model->username?>" id="usernmane" name = "username">
+    <div class="form-group mb-3">
+        <label for="usernmane">Username</label>
+        <input type="text" class="form-control <?php echo $model->hasError('username')?'is-invalid':'' ?>" value = "<?php echo $model->username ??'' ?>" id="usernmane" name = "username">
         <div class="invalid-feedback"><?php echo $model->getFirstError('username') ?></div>
     </div>
     
     <div class="form-group mb-3">
-        <label for="password">password</label>
-        <input type="password" class="form-control <?php echo $model->hasError('password')?'is-invalid':'' ?>" value = "<?php echo $model->password?>" id="password" rows="3" name = "password">
+        <label for="password">Password</label>
+        <input type="password" class="form-control <?php echo $model->hasError('password')?'is-invalid':'' ?>" value = "<?php echo $model->password ??'' ?>" id="password" rows="3" name = "password">
         <div class="invalid-feedback"><?php echo $model->getFirstError('password') ?></div>
     </div>
     
--- src/views/register.php
+++ src/views/register.php
@@ -1,35 +1,35 @@
 
-<h1 class="my-3">register</h1>
+<h1 class="my-3">Register</h1>
 
 <form action = "" method = "post">
 
     <div class="form-group mb-3">
-        <label for="name">name</label>
-        <input type="text" class="form-control <?php echo $model->hasError('name')?'is-invalid':'' ?>" value = "<?php echo $model->name?>" id="name" name = "name">
+        <label for="name">Name</label>
+        <input type="text" class="form-control <?php echo $model->hasError('name')?'is-invalid':'' ?>" value = "<?php echo $model->name??'' ?>" id="name" name = "name">
         <div class="invalid-feedback"><?php echo $model->getFirstError('name') ?></div>
     </div>
 
     <div class="form-group mb-3">
-        <label for="usernmane">username</label>
-        <input type="text" class="form-control <?php echo $model->hasError('username')?'is-invalid':'' ?>" value = "<?php echo $model->username?>" id="usernmane" name = "username">
+        <label for="usernmane">Username</label>
+        <input type="text" class="form-control <?php echo $model->hasError('username')?'is-invalid':'' ?>" value = "<?php echo $model->username ??'' ?>" id="usernmane" name = "username">
         <div class="invalid-feedback"><?php echo $model->getFirstError('username') ?></div>
     </div>
 
     <div class="form-group mb-3">
-        <label for="email">email</label>
-        <input type="email" class="form-control <?php echo $model->hasError('email')?'is-invalid':'' ?>" value = "<?php echo $model->email?>" id="email" rows="3" name = "email">
+        <label for="email">Email</label>
+        <input type="email" class="form-control <?php echo $model->hasError('email')?'is-invalid':'' ?>" value = "<?php echo $model->email ??'' ?>" id="email" rows="3" name = "email">
         <div class="invalid-feedback"><?php echo $model->getFirstError('email') ?></div>
     </div>
     
     <div class="form-group mb-3">
-        <label for="password">password</label>
-        <input type="password" class="form-control <?php echo $model->hasError('password')?'is-invalid':'' ?>" value = "<?php echo $model->password?>" id="password" rows="3" name = "password">
+        <label for="password">Password</label>
+        <input type="password" class="form-control <?php echo $model->hasError('password')?'is-invalid':'' ?>" value = "<?php echo $model->password ??'' ?>" id="password" rows="3" name = "password">
         <div class="invalid-feedback"><?php echo $model->getFirstError('password') ?></div>
     </div>
     
     <div class="form-group mb-3">
-        <label for="confirm_password">confirm password</label>
-        <input type="password" class="form-control <?php echo $model->hasError('confirm_password')?'is-invalid':'' ?>" value = "<?php echo $model->confirm_password?>" id="confirm_password" rows="3" name = "confirm_password">
+        <label for="confirm_password">Confirm password</label>
+        <input type="password" class="form-control <?php echo $model->hasError('confirm_password')?'is-invalid':'' ?>" value = "<?php echo $model->confirm_password ??'' ?>" id="confirm_password" rows="3" name = "confirm_password">
         <div class="invalid-feedback"><?php echo $model->getFirstError('confirm_password') ?></div>
     </div>
     
