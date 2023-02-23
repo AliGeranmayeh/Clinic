5cea0b6b77636e008d1787b4a0adde211844b15e
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-23 21:00:11 +0330

    Update Database.php

--- src/core/Database.php
+++ src/core/Database.php
@@ -14,6 +14,64 @@ class Database{
 
         $this->pdo = new \PDO($dsn , $username, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+    }
 
+    public function applyMigrations()
+    {
+        $this->createMigrationTable();
+        $applied_migrations=$this->getAppliedMigrations();
+
+        $new_migrations = [];
+        $file = scandir(__DIR__.'/../migrations');
+        $to_apply_migration = array_diff($file,$applied_migrations);
+        foreach ($to_apply_migration as  $migration) {
+            if ($migration === '.' || $migration === '..') {
+                continue;
+            }
+
+            require_once __DIR__."/../migrations/$migration";
+            $class_name =pathinfo($migration,PATHINFO_FILENAME);
+            $instance = new $class_name();
+            $this->log("Applying migration $migration");
+            $instance->up();
+            $this->log("Applied migration $migration");
+            $new_migrations[] = $migration;
+        }
+        if (!empty($new_migrations)) {
+            $this->saveMigrations($new_migrations);
+        }
+        else {
+            $this->log("All migrations are applied.");
+        }
+    }
+    public function createMigrationTable()
+    {
+        
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            ) ENGINE=INNODB;");
+    
+    }
+    public function getAppliedMigrations()
+    {
+        $stmt = $this->pdo->prepare('SELECT migration FROM migrations');
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_COLUMN);
+    }
+    public function saveMigrations(array $migrations)
+    {   
+        $str_migration = implode(',',array_map(fn($m)=>"('$m')",$migrations));
+        $stmnt= $this->pdo->prepare("
+        INSERT INTO migrations (migration) VALUES
+        $str_migration;
+        ");
+        $stmnt->execute();
+    }
+
+    protected function log($message)
+    {
+        echo '['.date('Y-m-d H:i:s').'] - '.$message.PHP_EOL;
     }
 }
