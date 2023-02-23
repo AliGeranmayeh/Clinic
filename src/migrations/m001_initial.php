e70e243557646b44799965db0afe63a7f3d1e7e9
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-23 18:37:25 +0330

    add structure of migrations

---
+++ src/migrations/m001_initial.php
@@ -0,0 +1,13 @@
+<?php 
+
+class m001_initial{
+
+    public function up()
+    {
+        echo 'applying migrations';   
+    }
+    public function down()
+    {
+        echo 'downinig migrations'; 
+    }
+}
