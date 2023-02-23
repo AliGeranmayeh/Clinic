70d28f61557640983d72a0d616bb521927861e71
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-23 10:32:46 +0330

    complete validate messages

--- src/core/Model.php
+++ src/core/Model.php
@@ -25,25 +25,42 @@ abstract class Model{
     public function validate()
     {
         foreach ($this->rules() as $attribute => $rules) {
-            $value = $this->{$attribute};
-            foreach ($rules as $ $rule) {
+            $value = $this->{$attribute}; 
+
+            foreach ($rules as $rule) {
+
                 $rule_name = $rule;
-                if(!is_string($rule_name)){
-                    $rule_name = $rule[0];
+                if(is_array($rule)){
+                    $rule_name = $rule[0]; 
                 }
-                if ($value == self::RULE_REQUIRED && !$value) {
+
+                if ($rule_name == self::RULE_REQUIRED && !$value) {
                     $this->addError($attribute,self::RULE_REQUIRED);
                 }
+                if ($rule_name == self::RULE_EMAIL && !filter_var($value,FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute,self::RULE_EMAIL);
+                }
+                if ($rule_name == self::RULE_MIN && strlen($value)<$rule['min']) {
+                    $this->addError($attribute,self::RULE_MIN,$rule);
+                }
+                if ($rule_name == self::RULE_MATCH && $this->{$rule['match']} !== $value) {
+                    $this->addError($attribute,self::RULE_MATCH,$rule);
+                }
             }
-                    
+              
         }
+        // var_dump($this->errors);
+        // die();
         return empty($this->errors);
             
     }
-    public function addError($attribute , $rule_name)
+    public function addError($attribute , $rule_name , $params = [])
     {
         $message = $this->errorMessages()?? '';
-        $this->errors[$attribute][] = $message;
+        foreach ($params as $key => $value) {
+            $message = str_replace("{{$key}}",$value,$message);
+        }
+        $this->errors[$attribute][] = $message[$rule_name];
     }
 
     public function errorMessages()
