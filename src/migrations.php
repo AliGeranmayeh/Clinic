ea0ac2a7d55df0a9d3b03372b18d7eb809af9e26
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-23 21:00:48 +0330

    update migration file

--- src/migrations.php
+++ src/migrations.php
@@ -1,2 +1,20 @@
+<?php 
+use clinic\core\Application;
+use clinic\controllers\SiteController;
+use clinic\controllers\AuthController;
 
+require_once './../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = [
+    'db' =>[
+        'dsn'=> $_ENV['DB_DSN'],
+        'username' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'] 
+    ]
+];
+$app = new Application($config);
+
+$app->db->applyMigrations();
--- src/public/index.php
+++ src/public/index.php
@@ -5,7 +5,7 @@ use clinic\controllers\AuthController;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__,));
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 $config = [
