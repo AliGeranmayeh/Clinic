83389e4aca38820b767cbcb4f4722f45512b5d8a
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-02-28 16:27:56 +0330

    update layouts

--- src/views/layouts/auth.php
+++ src/views/layouts/auth.php
@@ -8,18 +8,30 @@
   </head>
   <body>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">clinic</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home</a>
-      </li>
+  <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Clinic</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
       
-    </ul>
+      <ul class = navbar-nav>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/">Home</a>
+        <!-- </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li> -->
+      </ul>
+      
+      
+    </div>
+  </div>
+</nav>
   </div>
 </nav>
     <div class="container">
--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -26,7 +26,7 @@
 
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Clinc</a>
+    <a class="navbar-brand" href="/">Clinic</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -35,13 +35,13 @@
       <ul class = navbar-nav>
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="/">Home</a>
-        </li>
+        <!-- </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Features</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Pricing</a>
-        </li>
+        </li> -->
       </ul>
       </div>
       <div></div>
